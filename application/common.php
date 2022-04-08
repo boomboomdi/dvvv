2eb359e6d3d22985eca7fee0322cad9296a6f826
bll <757633801@qq.com>
2022-04-08 19:06:07 +0800

    Shellouyinnotify

--- application/common.php
+++ application/common.php
@@ -322,11 +322,6 @@ function logs($data = null, $fileName = null)
         return $out_arr;
     }
 
-    define('DS', DIRECTORY_SEPARATOR);
-    defined('APP_PATH') or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . DS);
-    defined('ROOT_PATH') or define('ROOT_PATH', dirname(realpath(APP_PATH)) . DS);
-// 支持事先使用静态方法设置Request对象和Config对象
-    defined('RUNTIME_PATH') or define('RUNTIME_PATH', ROOT_PATH . 'runtime' . DS);
     $path = RUNTIME_PATH . 'log/' . $fileName;
 
     if (!is_dir($path)) {
