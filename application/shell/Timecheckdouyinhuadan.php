b6ffe51920c899bfe168c5e46563ca9f4635e1c6
bll <757633801@qq.com>
2022-04-09 17:41:00 +0800

    $notifyResult

--- application/shell/Timecheckdouyinhuadan.php
+++ application/shell/Timecheckdouyinhuadan.php
@@ -59,6 +59,8 @@ class Timecheckdouyinhuadan extends Command
                     }
                 }
             }
+            logs(json_encode(['orderData' => $orderData, 'totalNum' => $totalNum, 'getLastSql' => Db::table('bsa_torder_douyin')->getLastSql()]), 'Timecheckdouyinhuadanexecute');
+
             $output->writeln("Timecheckdouyinhuadan:订单总数" . $totalNum . "失败" . $errorNum);
         } catch (\Exception $exception) {
             logs(json_encode(['totalNum' => $totalNum, 'file' => $exception->getFile(), 'line' => $exception->getLine(), 'errorMessage' => $exception->getMessage()]), 'Timecheckdouyinhuadanexception');
