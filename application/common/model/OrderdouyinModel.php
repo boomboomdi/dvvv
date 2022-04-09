b1ef8a82b1f821323169083556bce66bb8726a71
bll <757633801@qq.com>
2022-04-10 05:17:14 +0800

    Prepareorder

--- application/common/model/OrderdouyinModel.php
+++ application/common/model/OrderdouyinModel.php
@@ -479,7 +479,7 @@ class OrderdouyinModel extends Model
                         'url_status' => 2,
                         'notify_status' => 2,
                         'notify_time' => time(),
-                        'order_desc' => $notifyResult
+                        'order_desc' => "核销回调|".$notifyResult
                     ]);
                 Log::log('orderDouYinNotifyToWriteOffFail!', $notifyResult);
 
@@ -490,7 +490,7 @@ class OrderdouyinModel extends Model
                         'url_status' => 2,
                         'notify_status' => 1,
                         'notify_time' => time(),
-                        'order_desc' => $notifyResult
+                        'order_desc' => "核销回调|".$notifyResult
                     ]);
             }
             return modelReMsg('0', "", json_encode($notifyResult));
