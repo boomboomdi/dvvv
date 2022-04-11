0d5b323bb8ace97b643b62d40626a817db6e8492
bll <757633801@qq.com>
2022-04-12 02:03:06 +0800

    label-important

--- application/shell/Timecheckdouyin.php
+++ application/shell/Timecheckdouyin.php
@@ -41,8 +41,8 @@ class Timecheckdouyin extends Command
             //查询下单之前280s 到现在之前20s的等待付款订单
             $orderData = $orderdouyinModel->where('order_status', '<>', 1)
                 ->where('notify_status', '=', 0)
-//                ->where('url_status', '=', 2)
-                ->where('add_time', '<', $lockLimit)
+                ->where('url_status', '=', 2)
+//                ->where('add_time', '', $lockLimit)
                 ->select();
             logs(json_encode(['orderData' => $orderData, "sql" => Db::table("bsa_torder_douyin")->getLastSql(), "time" => date("Y-m-d H:i:s", time())]), 'Timecheckdouyin_list');
 
