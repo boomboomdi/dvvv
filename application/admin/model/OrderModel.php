0237161ea0fc9b0374022ab790e21cbbb737b351
bll <757633801@qq.com>
2022-04-12 17:25:33 +0800

    index/merchant data

--- application/admin/model/OrderModel.php
+++ application/admin/model/OrderModel.php
@@ -166,7 +166,6 @@ class OrderModel extends Model
             $where['merchant_sign'] = $merchantSign;
 
             $handTotalAmount = $this->field('sum(actual_amount) as order_total_amount')->where($where)->where("order_status", 5)->select();
-            logs(json_encode(['handTotalAmount' => $handTotalAmount, "last_sql" => Db::table('bsa_order')->getLastSql()]), 'merchantIndex_log_2');
 
             $where['status'] = 1;
             $totalAmount = $this->field('sum(actual_amount) as order_total_amount')->where($where)->select();
