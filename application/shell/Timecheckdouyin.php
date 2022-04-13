e7f38b8781b9512504247d9a5a51a85c4db33ca3
bll <757633801@qq.com>
2022-04-13 15:48:32 +0800

    last_use_time

--- application/shell/Timecheckdouyin.php
+++ application/shell/Timecheckdouyin.php
@@ -84,15 +84,15 @@ class Timecheckdouyin extends Command
                             logs(json_encode(['v' => $v, 'orderdouyinModelRes' => $orderdouyinModelRes, "sql" => Db::table("bsa_torder_douyin")->getLastSql(), "time" => date("Y-m-d H:i:s", time())]), 'orderdouyinModelRes_log2');
                         }
                     }
-                    if ((strtotime($v['limit_time']) - time()) > $limitTime) {
-                        $torderDouyinWhere['order_me'] = $v['order_me'];
-                        $torderDouyinWhere['order_pay'] = $v['order_pay'];
-                        $torderDouyinUpdate['order_status'] = 2;  ///匹配订单支付超时
-                        $torderDouyinUpdate['status'] = 2;  ///推单改为最终结束状态
-                        $torderDouyinUpdate['order_desc'] = "支付超时|准备回调核销失败";
-                        $orderdouyinModel->updateNotifyTorder($torderDouyinWhere, $torderDouyinUpdate);
-                        $orderdouyinModel->orderDouYinNotifyToWriteOff($v);
-                    }
+//                    if ((strtotime($v['limit_time']) - time()) > $limitTime) {
+//                        $torderDouyinWhere['order_me'] = $v['order_me'];
+//                        $torderDouyinWhere['order_pay'] = $v['order_pay'];
+//                        $torderDouyinUpdate['order_status'] = 2;  ///匹配订单支付超时
+//                        $torderDouyinUpdate['status'] = 2;  ///推单改为最终结束状态
+//                        $torderDouyinUpdate['order_desc'] = "支付超时|准备回调核销失败";
+//                        $orderdouyinModel->updateNotifyTorder($torderDouyinWhere, $torderDouyinUpdate);
+//                        $orderdouyinModel->orderDouYinNotifyToWriteOff($v);
+//                    }
                 }
 
             }
