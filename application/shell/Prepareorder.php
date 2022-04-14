02b3a5e754b35fb983aa5676d6fa974ccc342c6a
bll <757633801@qq.com>
2022-04-14 19:59:44 +0800

    order_status

--- application/shell/Prepareorder.php
+++ application/shell/Prepareorder.php
@@ -49,7 +49,7 @@ class Prepareorder extends Command
                         ->where('add_time', '>', time() - 600)
                         ->order("add_time asc")->count();
                     $v = $db::table("bsa_prepare_set")->where("id", $v['id'])->find();
-                    logs(json_encode(['num' => $can_use_num, 'amount' => $v['order_amount']]), 'prepareorderapicreateOrder_index_log');
+                    logs(json_encode(['num' => $can_use_num, 'amount' => $v['order_amount'], "sql" => $db::table("bsa_torder_douyin")->getLastSql()]), 'prepareorderapicreateOrder_index_log');
 
                     if (($v['prepare_num'] - $can_use_num) > 0) {
                         $res = $orderDouYinModel->createOrder($v, $v['prepare_num'] - $can_use_num);
