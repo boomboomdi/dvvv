b47ae40489bb3d0882d4fa8bf67b8b0463179198
bll <757633801@qq.com>
2022-04-25 19:50:13 +0800

    createOrder 时间限制

--- application/shell/Timecheckdouyinhuadan.php
+++ application/shell/Timecheckdouyinhuadan.php
@@ -49,7 +49,7 @@ class Timecheckdouyinhuadan extends Command
                 ->where('notify_status', '=', 0)
 //                ->where('url_status', '=', 2)
 //                ->where('add_time', '<', $LimitStartTime)   //时间是过了当前时间之前的15分
-                ->where('limit_time_1', '>', time())   //时间是过了当前时间之前的15分
+                ->where('limit_time_1', '<', time())   //当前时间  > 推单终止使用时间
                 ->select();
             logs(json_encode(['orderData' => $orderData, 'totalNum' => $totalNum, 'getLastSql' => Db::table('bsa_torder_douyin')->getLastSql()]), 'Timecheckdouyinhuadan_data_log');
 
